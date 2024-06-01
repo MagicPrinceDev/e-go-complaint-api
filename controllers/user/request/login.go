f0e3353c517fdb5cba6e30896ba0456e4ba12764
MagicPrinceDev <ferditutkun00@gmail.com>
2024-06-01 16:52:50 +0700

    Add json request

--- controllers/user/request/login.go
+++ controllers/user/request/login.go
@@ -3,8 +3,8 @@ package request
 import "e-complaint-api/entities"
 
 type Login struct {
-	Email    string `form:"email"`
-	Password string `form:"password"`
+	Email    string `form:"email" json:"email"`
+	Password string `form:"password" json:"password"`
 }
 
 func (r *Login) ToEntities() *entities.User {
