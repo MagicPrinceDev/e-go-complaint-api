dba05f0c3b70b17f89344c28dde21617944e1837
MagicPrinceDev <ferditutkun00@gmail.com>
2024-06-05 22:29:54 +0700

    Update Login Admin Response

--- controllers/admin/response/login.go
+++ controllers/admin/response/login.go
@@ -3,17 +3,19 @@ package response
 import "e-complaint-api/entities"
 
 type Login struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Token string `json:"token"`
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	Email        string `json:"email"`
+	IsSuperAdmin bool   `json:"is_super_admin"`
+	Token        string `json:"token"`
 }
 
 func LoginFromEntitiesToResponse(admin *entities.Admin) *Login {
 	return &Login{
-		ID:    admin.ID,
-		Name:  admin.Name,
-		Email: admin.Email,
-		Token: admin.Token,
+		ID:           admin.ID,
+		Name:         admin.Name,
+		Email:        admin.Email,
+		IsSuperAdmin: admin.IsSuperAdmin,
+		Token:        admin.Token,
 	}
 }
